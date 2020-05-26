d645376d21648c14a6fef385442ca6956ce6a089
xuanqizi <1318135982@qq.com>
2020-05-26 16:28:26 +0800

    准备写库存界面

---
+++ bookstore/frontend/src/components/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="menu">
+    <div class="title">
+      
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.menu{
+  width: 208px;
+  height: 959px;
+  border: none;
+}
+
+</style>
---
+++ bookstore/frontend/src/pages/Depot.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main">
+    <TitleBar></TitleBar>
+    <div class="page">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleBar from '@/components/TitleBar'
+
+export default {
+  
+}
+</script>
