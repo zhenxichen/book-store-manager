a60bec4a666ca1290957162f3cbb1149123f6ce0
xuanqizi <1318135982@qq.com>
2020-05-27 22:18:46 +0800

    添加零售查询界面

--- bookstore/frontend/src/pages/Querysell.vue
+++ bookstore/frontend/src/pages/Querysell.vue
@@ -3,7 +3,10 @@
     <TitleBar></TitleBar>
     <Menu></Menu>
     <div class="page">
-      
+      <div class="title">
+        <div class="title-text">零售查询</div>
+      </div>
+      <a-table :columns="columns" :data-source="data"></a-table>
     </div>
   </div>
 </template>
@@ -13,11 +16,40 @@ import TitleBar from "@/components/TitleBar";
 import Menu from "@/components/Menu";
 import axios from "axios";
 
+const columns = [
+  {
+    dataIndex: 'OrderID',
+    key: 'OrderID',
+    title: '订单号'
+  },
+  {
+    dataIndex: 'CustomerName',
+    key: 'CustomerName',
+    title: '顾客'
+  },
+  {
+    dataIndex: 'amount',
+    key: 'amount',
+    title: '金额'
+  },
+  {
+    dataIndex: 'operator',
+    key: 'operator',
+    title: '操作员'
+  },
+  {
+    dataIndex: 'time',
+    key: 'time',
+    title: '时间'
+  },
+]
+
 export default {
   name: 'Querysell',
   data() {
     return {
-
+      data: [],
+      columns
     }
   },
   created: function() {
@@ -30,9 +62,48 @@ export default {
       axios
         .get('/api/querysell/')
         .then((res) => {
-          console.log(res);
-        });
+          let resJson = JSON.parse(res.data.replace(/'/g, '"'));
+          let sellList = resJson.sellList;
+          this.data = sellList;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
     }
+  },
+  components: {
+    TitleBar,
+    Menu
   }
 }
 </script>
+
+<style scoped>
+
+.page {
+  width: 100%;
+  padding-top: 65px;
+  padding-left: 209px;
+}
+
+.title {
+  width: 100%;
+  height: 45px;
+  line-height: 20px;
+  background-color: rgba(240, 240, 242, 1);
+  text-align: center;
+  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.5);
+}
+
+.title-text {
+  width: 106px;
+  height: 20px;
+  color: rgba(144, 144, 144, 1);
+  font-size: 14px;
+  text-align: left;
+  font-family: Arial, Helvetica, sans-serif;
+  padding-left: 26px;
+  padding-top: 12px;
+}
+
+</style>
